4fea6ad66a2cfb226ad00684e32445280c19ac5c
puuu <puuu@users.noreply.github.com>
2019-05-11 15:11:16 +0900

    reintroduce update_on_boot parameter for my9231

--- esphome/components/my9231/__init__.py
+++ esphome/components/my9231/__init__.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.const import (CONF_BIT_DEPTH, CONF_CLOCK_PIN, CONF_DATA_PIN, CONF_ID,
-                           CONF_NUM_CHANNELS, CONF_NUM_CHIPS)
+                           CONF_NUM_CHANNELS, CONF_NUM_CHIPS, CONF_UPDATE_ON_BOOT)
 
 AUTO_LOAD = ['output']
 my9231_ns = cg.esphome_ns.namespace('my9231')
@@ -16,6 +16,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_NUM_CHANNELS, default=6): cv.All(cv.int_, cv.Range(min=3, max=1020)),
     cv.Optional(CONF_NUM_CHIPS, default=2): cv.All(cv.int_, cv.Range(min=1, max=255)),
     cv.Optional(CONF_BIT_DEPTH, default=16): cv.one_of(8, 12, 14, 16, int=True),
+    cv.Optional(CONF_UPDATE_ON_BOOT, default=True): cv.Coerce(bool),
 }).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -31,3 +32,4 @@ def to_code(config):
     cg.add(var.set_num_channels(config[CONF_NUM_CHANNELS]))
     cg.add(var.set_num_chips(config[CONF_NUM_CHIPS]))
     cg.add(var.set_bit_depth(config[CONF_BIT_DEPTH]))
+    cg.add(var.set_update(config[CONF_UPDATE_ON_BOOT]))
